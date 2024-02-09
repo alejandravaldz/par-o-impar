e197518d65237eadd87058a27e6d9f560789d9c9
User Name <User Name@LUPITA>
2024-02-09 09:41:40 -0500

    Agregar archivos de proyecto.

---
+++ par o impar/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("ingresar un numero entero");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine("el numero es par");
+
+}
+else
+{
+    Console.WriteLine(" el numero es impar");
+
+
+
+}
